f738a1343a9660d3553d42aa21ce412b6d1633de
YassineGabsi <gabsiyassine@gmail.com>
2021-09-02 15:13:52 +0100

    Add redirection to repos

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,10 +1,19 @@
-import React from "react";
+import React, {useState} from "react";
+import { useHistory } from "react-router-dom";
 import '../Assets/Home.scss';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import {faSearch, } from "@fortawesome/free-solid-svg-icons";
 import { faGithub } from '@fortawesome/free-brands-svg-icons';
 
 export default function Home() {
+
+    const [githubUser, setGithubUser] = useState('');
+    const history = useHistory();
+
+    const redirectToGithubRepositories = () => {
+        history.push('/' + githubUser);
+    }
+
 
     return (
         <div className={'home-container'}>
@@ -21,13 +30,13 @@ export default function Home() {
                     Enter your GitHub name to get all the repositories
                 </span>
                 <div className={'home-input-container'}>
-                    <input className={'home-input'} type="text" placeholder={'Ex: YassineGabsi'}/>
+                    <input className={'home-input'} type="text" placeholder={'Ex: YassineGabsi'} onChange={(e) => setGithubUser(e.target.value)}/>
                     <div className={'search-icon-container'}>
                         <FontAwesomeIcon className={'search-icon'} icon={ faSearch } color={'#192330'} />
                     </div>
                 </div>
 
-                <button className={'btn-custom btn-search'}>
+                <button className={'btn-custom btn-search'} onClick={redirectToGithubRepositories}>
                     Start searching !
                 </button>
 
