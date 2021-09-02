2b04bee0e953a1188f008b7912f719245a938770
YassineGabsi <gabsiyassine@gmail.com>
2021-09-02 12:03:13 +0100

    Configure home background + start adding input on home page

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -4,8 +4,19 @@ import '../Assets/Home.scss';
 export default function Home() {
 
     return (
-        <div>
-            home
+        <div className={'home-container'}>
+
+            <div className={'stars'}/>
+            <div className={'stars-second'}/>
+            <div className={'stars-third'}/>
+
+            <div className={'home-title-container'}>
+                <span>
+                    Enter your GitHub name to get all the repositories
+                </span>
+                <input className={'home-input'} type="text" placeholder={'Ex: YassineGabsi'}/>
+
+            </div>
         </div>
     );
 }
